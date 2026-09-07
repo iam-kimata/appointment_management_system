b63181149ec316e9f5491fa6e4014f8dd4a5ea72
iam-kimata <kimataaloyce444@gmail.com>
2026-09-07 22:14:03 +0300

    Login page API Consumption

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,17 +1,106 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import api from "../../api/axios";
+
 const Login = () => {
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+
+    const [errorMessage, setErrorMessage] = useState("");
+    const [errors, setErrors] = useState({});
+
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try{
+            const res = await api.post("login", {
+                email: formData.email,
+                password: formData.password,
+            });
+
+            localStorage.setItem("token", res.data.token);
+
+            navigate("/dashboard");
+
+        } catch (err) {
+            const status = err.response?.status;
+
+            if (status === 422) {
+                setErrors(err.response.data.errors || {});
+                setErrorMessage("");
+            } else if (status === 401) {
+                setErrorMessage(err.response?.data?.message || "Invalid Username or Password");
+                setErrors({});
+            } else {
+                setErrorMessage("Server error or Network issue");
+                console.log(err);
+            }
+        }
+    };
+
     return (
         <div className="d-flex">
             <div className="container py-5">
                 <div className="card login-card">
                     <h2 className="text-center mb-4 fw-bold">Login</h2>
-                    <form>
+
+                    {errorMessage && (
+                        <div className="alert alert-danger text-center py-2">
+                            {errorMessage}
+                        </div>
+                    )}
+
+                    <form onSubmit={handleSubmit}>
                         <div className="mb-3">
                             <label htmlFor="username" className="form-label">Username</label>
-                            <input type="text" name="email" id="email" className="form-control" placeholder="Enter Username" />
+
+                            {errors.email?.[0] (
+                                <div className="text-danger small mb-1">
+                                    {errors.email[0]}
+                                </div>
+                            )}
+
+                            <input 
+                                type="text" 
+                                name="email" 
+                                id="email" 
+                                className="form-control" 
+                                placeholder="Enter Username" 
+                                value={formData.email} 
+                                onChange={handleChange} 
+                            />
                         </div>
                         <div className="mb-3">
                             <label htmlFor="password" className="form-label">Password</label>
-                            <input type="password" name="password" id="password" className="form-control" placeholder="Enter Password" />
+
+                            {errors.password?.[0] (
+                                <div className="text-danger small mb-1">
+                                    {errors.password[0]}
+                                </div>
+                            )}
+
+                            <input 
+                                type="password" 
+                                name="password" 
+                                id="password" 
+                                className="form-control" 
+                                placeholder="Enter Password" 
+                                value={formData.password} 
+                                onChange={handleChange} 
+                            />
                         </div>
 
                         <button type="submit" className="btn btn-success w-100">Login</button>
